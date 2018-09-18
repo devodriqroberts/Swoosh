e89dc1370c9a194adb4b6251ca1e151c7a4ed833
DevoRoberts <devorob26@gmail.com>
2018-09-17 21:02:32 -0400

    Add league and experience view controllers with ui controls.

---
+++ swoosh-app/ExperienceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExperienceViewController.swift
+//  swoosh-app
+//
+//  Created by Devodriq Roberts on 9/17/18.
+//  Copyright © 2018 Devodriq Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
